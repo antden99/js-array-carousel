81fe2ef343db549eee58e1370ec835c7ff37c240
antden99 <antoniodenigris@icloud.com>
2024-02-23 17:45:11 +0100

    final commit wit bonus_1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 console.log("Tutto ok");
 
 //mi creo una lista di immagini
-const listaImg = ["01.webp","02.webp","03.webp","04.webp","05.webp"]
+const listaImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
 //mostro in console la lista delle immagini
 console.log(listaImg);
 
@@ -9,17 +9,17 @@ let activeImg = 0;
 
 const slides = document.querySelector(".slides");
 
-console.log(listaImg,activeImg,slides);
+console.log(listaImg, activeImg, slides);
 
 for (let index = 0; index < listaImg.length; index++) {
     const slide = listaImg[index];
     console.log(slide);
 
-    const markup = `<img class ="${ index === activeImg ? "active" : "" } " src="./assets/img/img/${ slide }" alt="" >`
+    const markup = `<img class ="${index === activeImg ? "active" : ""} " src="./assets/img/img/${slide}" alt="" >`
 
     console.log(markup);
 
-    slides.insertAdjacentHTML("beforeend",markup);
+    slides.insertAdjacentHTML("beforeend", markup);
 }
 
 
@@ -28,25 +28,31 @@ for (let index = 0; index < listaImg.length; index++) {
 const next = document.querySelector(".next");
 console.log(next);
 
-next.addEventListener("click",function(){
+next.addEventListener("click", function () {
+    //incremento activeImage per scorrere all'immagine successiva
+    activeImg++;
 
-    const selezionaSlide =document.querySelector(".active")
+    //aggiungo qui la condizione che se activeImg è posizionato a 0 deve andare all'ultima slide 
+    if (activeImg > listaImg.length - 1) {
+        activeImg = 0;
+    }
+
+
+    //seleziono la tramite la classe "active" il tag della slide corrente
+    const selezionaSlide = document.querySelector("img.active")
     console.log(selezionaSlide);
 
+    //tolgo la classe "active" alla slide corrente per poi riassegnarla alla successiva"
     selezionaSlide.classList.remove("active");
     console.log(selezionaSlide);
 
-    activeImg++;
-    console.log(activeImg);
 
     //qui mi vado a selezionare tutta la lista delle immagini che ha classe slides
     const listaSlides = document.querySelectorAll(".slides img");
-    
+
     //qui vado a selezionare il nodo successivo della dom list a cui assegno la classe "active"
     listaSlides[activeImg].classList.add("active");
 })
-
-
 
 
 
@@ -54,21 +60,33 @@ next.addEventListener("click",function(){
 const prev = document.querySelector(".prev");
 console.log(prev);
 
-prev.addEventListener("click",function(){
-
-    const selezionaSlide =document.querySelector(".active")
-    console.log(selezionaSlide);
-
-    selezionaSlide.classList.remove("active");
-    console.log(selezionaSlide);
-
+prev.addEventListener("click", function () {
+    //decremento activeImage per scorrere all'immagine precedente
     activeImg--;
     console.log(activeImg);
 
+    //aggiungo qui la condizione che se activeImg è posizionato alla fine delle slide deve andare alla prima slide
+
+    if (activeImg < 0) {
+        activeImg = listaImg.length - 1;
+
+    }
+
+    //seleziono la tramite la classe "active" il tag della slide corrente
+    const selezionaSlide = document.querySelector("img.active")
+    console.log(selezionaSlide);
+
+
+    //tolgo la classe "active" alla slide corrente per poi riassegnarla alla precedente"
+    selezionaSlide.classList.remove("active");
+    console.log(selezionaSlide);
+
+    
+
     //qui mi vado a selezionare tutta la lista delle immagini che ha classe slides
     const listaSlides = document.querySelectorAll(".slides img");
-    
+
     //qui vado a selezionare il nodo successivo della dom list a cui assegno la classe "active"
     listaSlides[activeImg].classList.add("active");
-    
+
 })
